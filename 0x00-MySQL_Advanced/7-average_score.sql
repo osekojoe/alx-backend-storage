8fddba3eeaeb49f1f29e86e2eba7a071a310e2ab
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-10 18:07:10 +0200

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,18 +7,15 @@
 --  existing users)
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-    
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-    
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
+
 END;
 //
 
-DELIMITER ;
+DELIMITER;
