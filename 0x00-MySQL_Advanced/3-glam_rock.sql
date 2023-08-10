0fd9cdb5a1dff93e9b9a0e8ac63b7f042f4954c6
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-10 16:46:42 +0200

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -6,5 +6,5 @@
 --  Your script can be executed on any database
 SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
