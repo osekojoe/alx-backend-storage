c41b9aa572215966dcd32f6f82ba79bf3cc17b8d
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-10 16:52:26 +0200

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,11 +3,6 @@
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-	UPDATE items
-	SET quantity = quantity - NEW.number
-	WHERE NAME = NEW.item_name;
-END;
-//
-
-DELIMITER;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE NAME = NEW.item_name;
