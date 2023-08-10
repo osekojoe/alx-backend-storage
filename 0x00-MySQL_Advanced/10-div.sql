3a4f2b0e984916d0fbf0600b327c207c20c83b5c
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-10 18:29:49 +0200

    Safe divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -11,15 +11,7 @@ DELIMITER //
 CREATE FUNCTION SafeDiv (a INT, B INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-	DECLARE result FLOAT;
-
-	IF b = 0 THEN
-		SET result = 0;
-	ELSE
-		SET result = a / b;
-	END IF;
-
-	RETURN result;
+	RETURN (IF(b = 0, 0, a / b));
 END;
 //
 
