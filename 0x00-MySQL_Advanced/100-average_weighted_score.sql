79eb97d4d20073e1ed33d0ccfeb944975e1622de
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-10 19:03:48 +0200

    Average weighted score

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,22 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that
+--  computes and store the average weighted score for a student.
+-- Requirements:
+--  Procedure ComputeAverageScoreForUser is taking 1 input:
+--  user_id, a users.id value (you can assume user_id is linked to an existing users)
+-- Tips:
+--  Calculate-Weighted-Average
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
+BEGIN
+	UPDATE users set average_score = (
+	SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight))
+	FROM corrections
+	INNER JOIN projects
+	ON projects.id = corrections.project_id
+	WHERE corrections.user_id = user_id
+	WHERE users.id = user_id;
+END;
+//
+
+DELIMITER;
