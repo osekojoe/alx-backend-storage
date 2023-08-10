5ac762f59275952ef0fbea8bc7375174b9fa9fab
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-10 17:55:45 +0200

    Add bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -16,7 +16,10 @@ BEGIN
     WHERE NOT EXISTS(SELECT * FROM projects WHERE name = project_name LIMIT 1);
 
     INSERT INTO corrections(user_id, project_id, score) 
-    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+    VALUES (
+    user_id,
+    (SELECT id FROM projects WHERE name = project_name),
+    score);
 END;
 //
 
